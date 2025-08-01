7b0d3860957078f953bb9a221cee2d93489a7f2a
Avishkar-uss <avishkarmore28@gmail.com>
2025-08-01 11:41:52 +0530

    Update App.tsx

--- Sweetheart1/client/src/App.tsx
+++ Sweetheart1/client/src/App.tsx
@@ -4,11 +4,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import LoveApp from "@/components/LoveApp";
+import AdminPanel from "@/components/AdminPanel"; 
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={LoveApp} />
+      <Route path="/admin" component={AdminPanel} /> 
       <Route path="/:section" component={LoveApp} />
     </Switch>
   );
